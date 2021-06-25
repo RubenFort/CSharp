193168f039f0dd82dd8ee5e823cab12d8f4a498c
Ruben <rdfortu@gmail.com>
2021-06-25 18:10:11 +0200

    Ejemplo sencillo static

---
+++ 02StaticStatement/02StaticStatement/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02StaticStatement
+{
+    class Student
+    {
+        public static int studentNumber { get; set; }
+        public static void getAddNote()
+        {
+            MessageBox.Show("Static");
+        }
+    }
+}

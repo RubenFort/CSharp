15daa88929bd4eef365fa5683f0febbdebfb2d2c
Ruben <rdfortu@gmail.com>
2021-06-25 20:32:42 +0200

    Polimorfismo clases abstractas

---
+++ 06AbstractClass/06AbstractClass/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace _06AbstractClass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShow = new System.Windows.Forms.Button();
+            this.textBoxShow = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnShow
+            // 
+            this.btnShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShow.Location = new System.Drawing.Point(330, 51);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(126, 49);
+            this.btnShow.TabIndex = 0;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // textBoxShow
+            // 
+            this.textBoxShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxShow.Location = new System.Drawing.Point(63, 51);
+            this.textBoxShow.Multiline = true;
+            this.textBoxShow.Name = "textBoxShow";
+            this.textBoxShow.Size = new System.Drawing.Size(203, 306);
+            this.textBoxShow.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 419);
+            this.Controls.Add(this.textBoxShow);
+            this.Controls.Add(this.btnShow);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.TextBox textBoxShow;
+    }
+}
+
---
+++ 06AbstractClass/06AbstractClass/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _06AbstractClass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        abstract class Vehicle
+        {
+            public abstract double getFuel();
+        }
+
+        class Bus : Vehicle
+        {
+            public override double getFuel()
+            {
+                return 300;
+            }
+        }
+
+        class Truck : Vehicle
+        {
+            public override double getFuel()
+            {
+                return 500;
+            }
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            string text = "";
+            Vehicle vc;
+            vc = new Bus();
+            double busFuel = vc.getFuel();
+            text += "Bus Fuel Amount is: " + busFuel.ToString() + Environment.NewLine;
+            vc = new Truck();
+            double truckFuel = vc.getFuel();
+            text += "Truck Fuel Amount is: " + busFuel.ToString() + Environment.NewLine;
+            textBoxShow.Text = text;
+        }
+    }
+}

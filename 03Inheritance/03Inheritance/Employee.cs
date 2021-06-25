fbca11687c0b8ca8e83f9d842fb32a0cdbf59d4c
Ruben <rdfortu@gmail.com>
2021-06-25 18:35:34 +0200

    Crear clases y herencias

---
+++ 03Inheritance/03Inheritance/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03Inheritance
+{
+    class Employee:Human
+    {
+        protected string position { get; set; }
+        protected double salary { get; set; }
+
+        public void setValues()
+        {
+            name = "Charles";
+            id = 1;
+            gender = 'M';
+            age = 30;
+        }
+    }
+}
---
+++ 03Inheritance/03Inheritance/Teacher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03Inheritance
+{
+    class Teacher:Employee
+    {
+        public string branch { get; set; }
+
+        public void findSalary()
+            {
+                salary = 2000;
+            }
+
+        public void setValues2()
+        {
+            position = "Teacher";
+            branch = "Match";
+        }
+    }
+}

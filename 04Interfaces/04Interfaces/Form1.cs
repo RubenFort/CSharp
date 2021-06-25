c494427b8ad401cdf4487a3c7424f636d0abb0fd
Ruben <rdfortu@gmail.com>
2021-06-25 19:11:09 +0200

    Crear e implementar Interface

---
+++ 04Interfaces/04Interfaces/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _04Interfaces
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        interface Computer
+        {
+            void getDesktops();
+            void getLaptops();
+        }
+
+        class DellComputer : Computer
+        {
+            public string name { get; set; }
+            public double price { get; set; }
+
+            public void getDesktops()
+            {
+                name = "Desktop01";
+                price = 1500;
+            }
+
+            public void getLaptops()
+            {
+                name = "Laptop01";
+                price = 1200;
+            }
+        }
+    }
+}

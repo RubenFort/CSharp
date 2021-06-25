f45bcfc27b52f63a0b27d1842fe386cff56ac52d
Ruben <rdfortu@gmail.com>
2021-06-25 21:09:48 +0200

    Declaracion clases parciales

---
+++ 07PartialClass/07PartialClass/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace _07PartialClass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShow = new System.Windows.Forms.Button();
+            this.textBoxShow = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnShow
+            // 
+            this.btnShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShow.Location = new System.Drawing.Point(381, 47);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(96, 51);
+            this.btnShow.TabIndex = 0;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            // 
+            // textBoxShow
+            // 
+            this.textBoxShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxShow.Location = new System.Drawing.Point(73, 47);
+            this.textBoxShow.Multiline = true;
+            this.textBoxShow.Name = "textBoxShow";
+            this.textBoxShow.Size = new System.Drawing.Size(237, 312);
+            this.textBoxShow.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 435);
+            this.Controls.Add(this.textBoxShow);
+            this.Controls.Add(this.btnShow);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.TextBox textBoxShow;
+    }
+}
+
---
+++ 07PartialClass/07PartialClass/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _07PartialClass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public partial class Employee
+        {
+            public int employeeId { get; set; }
+            public string name { get; set; }
+            public double salary { get; set; }
+        }
+
+        public partial class Employee
+        {
+            public void setValues()
+            {
+                name = "Charles";
+                employeeId = 1;
+                salary = 2000;
+            }
+        }
+    }
+}

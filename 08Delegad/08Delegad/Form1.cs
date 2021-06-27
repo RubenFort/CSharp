9bc509aa26651c84d7e47803af17c9e96601a8cf
Ruben <rdfortu@gmail.com>
2021-06-27 19:05:13 +0200

    ejemplos enumerados y delegados

---
+++ 08Delegad/08Delegad/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _08Delegad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public delegate void fillTextBox(int x);
+        string textValues = "";
+
+        public void fillLanguageNote(int ExamNote)
+        {
+            textValues += "Language note: " + ExamNote + Environment.NewLine;
+        }
+
+        public void fillMathNote(int ExamNote)
+        {
+            textValues += "Math note: " + ExamNote + Environment.NewLine;
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            fillTextBox fillText = new fillTextBox(fillMathNote);
+            fillText += fillLanguageNote; //Añado una segunda función con +,
+            fillText(10);
+            fillText -= fillMathNote;//Con - quito una función
+            fillText(8);
+            showInTextBox();
+        }
+
+        public void showInTextBox()
+        {
+            textBox1.Text = textValues;
+        }
+    }
+}
---
+++ 08Enumerados/08Enumerados/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _08Enumerados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        enum colors { red, blue, white = 5, black };
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            //Al parsear un enum a int, obtenemos el valor de su indice
+            String text = "Red: " + (int)colors.red + Environment.NewLine;
+            text += "Blue: " + (int)colors.blue + Environment.NewLine;
+            text += "White: " + (int)colors.white + Environment.NewLine;
+            text += "Black: " + (int)colors.black + Environment.NewLine;
+            text += "**********************" + Environment.NewLine; ;
+
+            //GetValues devuelve el valore numérico de cada enum
+            foreach (int i in Enum.GetValues(typeof(colors))){
+                text += i + Environment.NewLine;
+            }
+            text += "**********************" + Environment.NewLine; ;
+
+            //GetNames devuelve el nombre de cada enum
+            foreach (string str in Enum.GetNames(typeof(colors)))
+            {
+                text += str + Environment.NewLine;
+            }
+
+            textBox1.Text = text;
+        }
+
+    }
+}

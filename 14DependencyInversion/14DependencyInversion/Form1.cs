d29629d77732aac03393c40326d1b10240885f83
Ruben <rdfortu@gmail.com>
2021-07-06 04:21:24 +0200

    Principio SOLID => (D)ependency Inversion part01, clases dependientes(mal codigo)

---
+++ 14DependencyInversion/14DependencyInversion/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _14DependencyInversion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class HoldFileLogs
+        {
+            public string Message { get; set; }
+            public void LogContext()
+            {
+
+            }
+        }
+
+        class HoldDBLogs
+        {
+            public string Message { get; set; }
+            public void LogContext()
+            {
+         
+            }
+        }
+
+        class ManageLogs
+        {
+            private HoldFileLogs holdFileLogs;
+            private HoldDBLogs holdDBLogs;
+            public ManageLogs()
+            {
+                holdFileLogs = new HoldFileLogs();
+                holdDBLogs = new HoldDBLogs();
+            }
+
+            public void LogContext()
+            {
+                holdFileLogs.LogContext();
+                holdDBLogs.LogContext();
+            }
+        }
+    }
+}
